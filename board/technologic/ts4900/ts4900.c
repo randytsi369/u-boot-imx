b3bdf85550a602a23fbe9245f5e962413c7e7164
Mark Featherston <mark@embeddedarm.com>
2014-10-15 10:39:36 -0700

    Removed TS-4900 specific ice40 code

--- board/technologic/ts4900/ts4900.c
+++ board/technologic/ts4900/ts4900.c
@@ -362,67 +362,6 @@ void setup_fpga(void)
 	imx_iomux_v3_setup_multiple_pads(fpga_pads, ARRAY_SIZE(fpga_pads));	
 }
 
-static int do_ice40_load(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
-{
-	unsigned int len, data;
-	struct spi_slave *slave;
-	int ret_val, i;
-
-	// Parse image from mkimage
-	data = simple_strtoul(argv[1], NULL, 16);
-	len = simple_strtoul(argv[2], NULL, 16);
-
-	slave = spi_setup_slave(1, 0, 25000000, SPI_MODE_1);
-	if(spi_claim_bus(slave)){
-		printf("Failed to claim the SPI bus\n");
-		return 1;
-	}
-
-	// We need to pulse reset while the clock is high, so set it to a gpio first
-	imx_iomux_v3_setup_pad(
-		MX6_PAD_CSI0_DAT8__GPIO_5_26 | MUX_PAD_CTRL(SPI_PAD_CTRL));
-	gpio_direction_input(IMX_GPIO_NR(5, 20)); // fpga_done
-	gpio_direction_output(IMX_GPIO_NR(5, 21), 0); // reset low
-	gpio_direction_output(IMX_GPIO_NR(5, 26), 1); // spi_clk high
-	gpio_direction_output(IMX_GPIO_NR(6, 2), 0); // spi cs# low
-	udelay(800);
-	gpio_set_value(IMX_GPIO_NR(5, 21), 1); // reset high
-	udelay(800);
-	imx_iomux_v3_setup_pad(
-		MX6_PAD_CSI0_DAT8__ECSPI2_SCLK | MUX_PAD_CTRL(SPI_PAD_CTRL));
-
-	ret_val = spi_xfer(slave, len * 8, (void *)data, NULL, 0);
-	udelay(800);
-
-	// FPGA requires additional spi clocks after bitstream
-	char zeroes[100];
-	memset(zeroes, 0, 100);
-	ret_val = spi_xfer(slave, 100 * 8, zeroes, NULL, 0);
-
-	gpio_set_value(IMX_GPIO_NR(6, 2), 1); // spi cs# high
-
-	for(i = 0; i <= 3000; i++)
-	{
-		if(gpio_get_value(IMX_GPIO_NR(5, 20)))
-			break;
-		if(i == 3000){ 
-			printf("FPGA_DONE never asserted\n");
-			ret_val = 1;
-		}
-		udelay(1000);
-	}
-
-	spi_release_bus(slave);
-
-	return ret_val;
-}
-
-U_BOOT_CMD(ice40, 3, 0, do_ice40_load,
-	"ICE40 programming support",
-	" [image address] [filesize]\n"
-	"    Image must be in mkimage legacy format\n"
-);
-
 struct i2c_pads_info i2c_pad_info0 = {
 	.scl = {
 		.i2c_mode  = MX6_PAD_EIM_D21__I2C1_SCL | MUX_PAD_CTRL(I2C_PAD_CTRL),
