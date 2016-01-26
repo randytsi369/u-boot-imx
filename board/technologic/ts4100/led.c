3965f83f3f24513875628ea995518696c1475818
Mark Featherston <mark@embeddedarm.com>
2016-01-25 17:29:23 -0700

    Added temporary regs for LEDs

--- board/technologic/ts4100/led.c
+++ board/technologic/ts4100/led.c
@@ -8,9 +8,9 @@ static unsigned int saved_state[3] = {STATUS_LED_OFF,
 void red_led_on(void)
 {
 	uint8_t val;
-	i2c_read(0x28, 0, 2, &val, 1);
+	i2c_read(0x28, 59, 2, &val, 1);
 	val &= ~(1 << 2);
-	i2c_write(0x28, 0, 2, &val, 1);
+	i2c_write(0x28, 59, 2, &val, 1);
 
 	saved_state[STATUS_LED_RED] = STATUS_LED_ON;
 }
@@ -18,9 +18,9 @@ void red_led_on(void)
 void red_led_off(void)
 {
 	uint8_t val;
-	i2c_read(0x28, 0, 2, &val, 1);
+	i2c_read(0x28, 59, 2, &val, 1);
 	val |= (1 << 2);
-	i2c_write(0x28, 0, 2, &val, 1);
+	i2c_write(0x28, 59, 2, &val, 1);
 
 	saved_state[STATUS_LED_RED] = STATUS_LED_OFF;
 }
@@ -28,9 +28,10 @@ void red_led_off(void)
 void green_led_on(void)
 {
 	uint8_t val;
-	i2c_read(0x28, 0, 2, &val, 1);
+	i2c_read(0x28, 59, 2, &val, 1);
 	val &= ~(1 << 1);
-	i2c_write(0x28, 0, 2, &val, 1);
+	
+	i2c_write(0x28, 59, 2, &val, 1);
 
 	saved_state[STATUS_LED_GREEN] = STATUS_LED_ON;
 }
@@ -38,9 +39,9 @@ void green_led_on(void)
 void green_led_off(void)
 {
 	uint8_t val;
-	i2c_read(0x28, 0, 2, &val, 1);
+	i2c_read(0x28, 59, 2, &val, 1);
 	val |= (1 << 1);
-	i2c_write(0x28, 0, 2, &val, 1);
+	i2c_write(0x28, 59, 2, &val, 1);
 
 	saved_state[STATUS_LED_GREEN] = STATUS_LED_OFF;
 }
@@ -48,7 +49,7 @@ void green_led_off(void)
 void __led_init(led_id_t mask, int state)
 {
 	/* LEDs are all off of the FPGA I2C Regs */
-	i2c_set_bus_num(1);
+	i2c_set_bus_num(2);
 
 	__led_set(mask, state);
 }
