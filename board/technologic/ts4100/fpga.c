6ff61c3cdeb88c6a74153a4a04211d25bb848bcc
Mark Featherston <mark@embeddedarm.com>
2016-03-24 17:24:54 -0700

    Force i2c bus

--- board/technologic/ts4100/fpga.c
+++ board/technologic/ts4100/fpga.c
@@ -4,8 +4,8 @@
 int fpga_get_rev(void)
 {
 	uint8_t val = 0;
+	i2c_set_bus_num(2);
 	i2c_read(0x28, 306, 2, &val, 1);
-
 	return val;
 }
 
@@ -17,7 +17,7 @@ void fpga_gpio_output(int io, int value)
 	val = 0x1;
 	// DATA
 	if(value) val |= 0x2;
-
+	i2c_set_bus_num(2);
 	i2c_write(0x28, io, 2, &val, 1);
 }
 
@@ -25,6 +25,7 @@ int fpga_gpio_input(int io)
 {
 	uint8_t val = 0;
 	// Set input with 0
+	i2c_set_bus_num(2);
 	i2c_write(0x28, io, 2, &val, 1);
 	i2c_read(0x28, io, 2, &val, 1);
 	return (val & 0x4) ? 1 : 0;
