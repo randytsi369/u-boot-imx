67c997375ec57f79b8087c58cea07d930a34ea36
Mark Featherston <mark@embeddedarm.com>
2016-03-28 14:10:54 -0700

    Implemented baseboard id detection

--- board/technologic/ts4100/ts4100.c
+++ board/technologic/ts4100/ts4100.c
@@ -470,8 +470,22 @@ static const struct boot_mode board_boot_modes[] = {
 static void do_bbdetect(void)
 {
 	int id = 0;
-	id = 2;
+	int i;
 
+	for(i = 0; i < 8; i++) {
+		if(i & 1)fpga_gpio_output(RED_LED_PADN, 1);
+		else fpga_gpio_output(RED_LED_PADN, 0);
+
+		if(i & 2)fpga_gpio_output(GREEN_LED_PADN, 1);
+		else fpga_gpio_output(GREEN_LED_PADN, 0);
+		
+		if(i & 4)fpga_gpio_output(DIO_20, 1);
+		else fpga_gpio_output(DIO_20, 0);
+
+		id = (id >> 1);
+		if(fpga_gpio_input(DIO_05)) id |= 0x80;
+	}
+	printf("Baseboard ID: 0x%X, Rev %d\n", id & ~0xc0, ((id & 0xc0) >> 6));
 	setenv_hex("baseboardid", id & ~0xc0);
 	setenv_hex("baseboardrev", ((id & 0xc0) >> 6));
 }
