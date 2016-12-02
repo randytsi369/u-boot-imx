edc9e035aac31b73d06e9fa4b57aa3534d751109
Mark Featherston <mark@embeddedarm.com>
2016-12-02 15:56:04 -0700

    Removed redundant output, removed extra whitespace.

--- common/autoboot.c
+++ common/autoboot.c
@@ -329,10 +329,8 @@ const char *bootdelay_process(void)
 #ifdef is_boot_from_usb
 	if (is_boot_from_usb()) {
 		disconnect_from_pc();
-		printf("Boot from USB for mfgtools\n");
 		bootdelay = 0;
-		set_default_env("Use default environment for \
-				 mfgtools\n");
+		set_default_env("Use default environment for mfgtools\n");
 	} else {
 		printf("Normal Boot\n");
 	}
