6d9df394562ee6acbfce9114ce2f01872b9fb15f
Kris Bahnsen <kris@embeddedarm.com>
2018-02-06 16:02:36 -0800

    TS-4100: Added support for waiting until TS-SILO caps are charged

--- board/technologic/ts4100/silabs.c
+++ board/technologic/ts4100/silabs.c
@@ -5,6 +5,7 @@
  */
 
 #include <common.h>
+#include <console.h>
 #include <stdlib.h>
 #include <asm/io.h>
 #include <asm/arch/gpio.h>
@@ -92,10 +93,43 @@ void read_adcs(void)
 	printf("VDD_ARM_CAP=%d\n", sscale(data[12]));
 }
 
+int wait_for_supercaps(int pct, int verbose)
+{
+	unsigned char buf[4] = {0};
+	unsigned int check;
+
+	enable_tssilo();
+
+	if(pct == 0) {
+		printf("Not waiting for SuperCaps to charge\n");
+		return 0;
+	} else {
+		printf("Waiting until SuperCaps are charged to %d%%\n", pct);
+	}
+	if(pct > 100) pct = 100;
+
+	while(1) {
+		i2c_read(0x4a, 0x0, 0, &buf[0], 4);
+		check = (((buf[2]<<8|buf[3])*100/237));
+		if(check > 311) {
+			check = check-311;
+			if(check > 100) check = 100;
+			if(verbose) printf("%d%%\n", check);
+			if(check >= pct) return 0;
+		} else {
+			if(verbose) printf("0%%\n");
+		}
+		if(ctrlc()) return 1;
+		udelay(1000000);
+	}
+}
+
 static int do_microctl(cmd_tbl_t *cmdtp, int flag, 
 	int argc, char * const argv[])
 {
 	int i;
+	unsigned int pct = 0;
+	unsigned int verbose = 0;
 	i2c_set_bus_num(0);
 
 	for (i = 1; i < argc; i++)
@@ -120,7 +154,18 @@ static int do_microctl(cmd_tbl_t *cmdtp, int flag,
 				printf("Sleep for %d seconds\n", micros);
 				board_sleep(micros);
 				break;
-
+			case '0':
+				break;
+			case '1':
+				verbose = 1;
+				break;
+			case 'w':
+				if (i+1 == argc) {
+					printf("Missing argument for percent to charge\n");
+					return 1;
+				}
+				pct = simple_strtoul(argv[++i], NULL, 10);
+				break;
 			case 'e':
 				enable_tssilo();
 				break;
@@ -133,14 +178,18 @@ static int do_microctl(cmd_tbl_t *cmdtp, int flag,
 		}
 	}
 
+	if (pct) wait_for_supercaps(pct, verbose);
+
 	return 0;
 }
 
-U_BOOT_CMD(tsmicroctl, 3, 0, do_microctl,
+U_BOOT_CMD(tsmicroctl, 4, 0, do_microctl,
 	"TS supervisory microcontroller access",
 	"  Usage: tsmicroctl <options>\n"
 	"    -i           Print information\n"
 	"    -s <seconds> Sleep for <seconds>\n"
-        "    -e           Enable charging of TS-SILO supercaps\n"
-        "    -d           Disable charging of TS-SILO supercaps\n"
+	"    -w <pct>     Wait until TS-SILO supercaps are at <pct>% charge\n"
+	"    -1           Verbose output when -w is supplied\n"
+	"    -e           Enable charging of TS-SILO supercaps\n"
+	"    -d           Disable charging of TS-SILO supercaps\n"
 );
