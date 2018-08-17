a9602f8cd51bb81ee959ea6647e1b33e008020c8
Kris Bahnsen <kris@embeddedarm.com>
2018-08-17 13:09:21 -0700

    TS-4100: Modified FPGA GPIO routines to do proper RMW to prevent IO glitches

--- board/technologic/ts4100/fpga.c
+++ board/technologic/ts4100/fpga.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 Technologic Systems
+ * Copyright (C) 2018 Technologic Systems
  *
  * SPDX-License-Identifier:	GPL-2.0+
  */
@@ -17,24 +17,46 @@ int fpga_get_rev(void)
 	return val;
 }
 
+/* We need to to a RMW here. Read the current OE state, write that and the new
+ * value to the reg, and then if OE was not set, set it on a separate write.
+ * If this is not done, it is possible that the output state can change
+ * non-deterministically in relation to OE.
+ */
 void fpga_gpio_output(int io, int value)
 {
 	uint8_t val;
 
-	// OE
-	val = 0x1;
-	// DATA
-	if(value) val |= 0x2;
 	i2c_set_bus_num(2);
+
+	// DATA
+	i2c_read(0x28, io, 2, &val, 1);
+	if(value) val |= 0x2;
+	else val &= ~(0x2);
 	i2c_write(0x28, io, 2, &val, 1);
+
+	// OE
+	if(!(val & 0x1)) { //Only update OE if currently not an output
+		val |= 0x1;
+		i2c_write(0x28, io, 2, &val, 1);
+	}
 }
 
+/* We need to do a RMW, and then read the data on the input.
+ * We want to keep the output state while changing the pin to an input.
+ * If we don't, it is possible to change the state of the output before OE is
+ * set. This is a bad thing to happen.
+ */
 int fpga_gpio_input(int io)
 {
-	uint8_t val = 0;
-	// Set input with 0
+	uint8_t val;
+
 	i2c_set_bus_num(2);
-	i2c_write(0x28, io, 2, &val, 1);
+
 	i2c_read(0x28, io, 2, &val, 1);
+	if(val & 0x1) { //Only do the RMW if the pin is an output
+		val &= ~(0x1); //Clear the OE bit to set as input
+		i2c_write(0x28, io, 2, &val, 1);
+		i2c_read(0x28, io, 2, &val, 1);
+	}
 	return (val & 0x4) ? 1 : 0;
 }
